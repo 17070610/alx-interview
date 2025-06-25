95ddec92117033a3dd27079a5bce937e29a6bd5a
17070610 <schimea@hotmail.com>
2025-06-25 21:48:56 +0200

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,54 @@
 #!/usr/bin/python3
-"""Prime game module"""
+"""Prime Game Module"""
+
 
 def isWinner(x, nums):
-    if x < 1 or not nums:
-        return None
-
-    n = max(nums)
-    sieve = [True for _ in range(n + 1)]
-    sieve[0:2] = [False, False]
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-
-    primes_count = [0] * (n + 1)
-    count = 0
-    for i in range(n + 1):
-        if sieve[i]:
-            count += 1
-        primes_count[i] = count
-
-    maria_wins = 0
-    ben_wins = 0
+    mariaWinsCount = 0
+    benWinsCount = 0
 
     for num in nums:
-        if primes_count[num] % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
+        roundsSet = list(range(1, num + 1))
+        primesSet = primes_in_range(1, num)
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        if not primesSet:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while(True):
+            if not primesSet:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primesSet.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+            isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Winner: Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Winner: Ben"
+
+    return None
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    primes = [n for n in range(start, end+1) if is_prime(n)]
+    return primes
